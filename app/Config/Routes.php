dfbad11679c2c3b0cf98d6f29aab09875ac46f88
cristofer <cristofer.mondaca@alu.ucm.cl>
2025-07-24 20:38:11 -0400

    rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('chatbot/clientes-ano', 'Chatbot::clientesAno');
 
 $routes->get('/chat', 'Chat::index');
 $routes->post('/chat/consultar', 'Chat::consultar');
+$routes->get('/chat/historial', 'Chat::historial');
+
